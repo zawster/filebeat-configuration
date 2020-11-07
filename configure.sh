15ba9b8b63f8d1389a4d2180687b30e096f970b9
zawster <p176142@nu.edu.pk>
2020-11-07 18:31:55 +0500

    Configuration script to configure filebeat

---
+++ configure.sh
@@ -0,0 +1,19 @@
+
+
+sudo docker pull docker.elastic.co/beats/filebeat:7.9.3
+
+sudo docker run \
+docker.elastic.co/beats/filebeat:7.9.3 \
+setup -E setup.kibana.host/Your_IP:5601 \
+-E output.elasticsearch.hosts=["Your_IP:9200"]
+
+
+sudo docker run -d \
+	--name=filebeat \
+	--user=root \
+	--volume="$(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
+	--volume="/var/log/logs_dataset:/var/log/custom:ro" \
+docker.elastic.co/beats/filebeat:7.9.3 filebeat -e -strict.perms=false \
+-E output.elasticsearch.hosts=["Your_IP:9200"]
+
+
